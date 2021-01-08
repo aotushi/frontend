6e3d30df9ed35a934d6e035168a42747735ed712
vsol <vspb@qq.com>
2021-01-08 22:38:20 +0800

    change folder structor

---
+++ React/README.md
@@ -0,0 +1,4 @@
+### 1.异步action
+#### 1.1 action中关于异步的处理:return+箭头函数(setTimeout(dispatch({})))
+
+#### 1.2 入口文件index.js中使用Provider,无法更新页面的state
--- src_2_两组件数据共享_redux集中式管理/redux/store.js
+++
@@ -1,6 +0,0 @@
-import {createStore, applyMiddleware} from 'redux';
-import thunk  from 'redux-thunk';
-import allReducer from './reducers'
-
-
-export default createStore(allReducer, applyMiddleware(thunk))
